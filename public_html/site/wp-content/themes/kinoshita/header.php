915cb77b111567aacbda2b9ce53795e3bbf03523
mieli-hayashi <hayashi@mieli.co.jp>
2025-01-21 17:10:16 +0900

    PC版グッドデザイン賞ページ作成

--- public_html/site/wp-content/themes/kinoshita/header.php
+++ public_html/site/wp-content/themes/kinoshita/header.php
@@ -55,6 +55,7 @@
 
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/good-design/good-design.css">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.png">
 	<link rel="icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.svg">
